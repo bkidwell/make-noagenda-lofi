cbe5300ef3655e4e38e7dfddd54f2e7bd07af784
Brendan Kidwell <snarf@glump.net>
2014-06-20 02:37:01 -0400

    always wait 5 seconds before taking action; scan for new files at startup

--- scripts/watch-for-files.sh
+++ scripts/watch-for-files.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 app_dir="$(readlink -f "$(dirname $0)/..")"
+run="$app_dir/scripts/process.py"
 
 echo $$>"$app_dir/state/watch-for-files.pid"
 
+sleep 5
+"$run"
+
 while true; do
   inotifywait --quiet --event create --event move "$app_dir/input" && (
-    "$app_dir/scripts/process.py"
+    sleep 5
+    "$run"
   )
 done
